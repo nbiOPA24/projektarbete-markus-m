e0c66b041eae4fd6453e7d61aa8c3610e4beccad
Markus Merits <markus.merits@hotmail.se>
2024-11-04 12:47:07 +0100

    added .gitignore asfirst commit

--- Program.cs
+++ Program.cs
@@ -1,12 +1,46 @@
-﻿class Program
+﻿using System;
+
+class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine($"x: ", x, " y: ", y);
+        Random rnd = new Random();
+        string textInput;
         int x = 0;
         int y = 0;
+        Console.WriteLine($"x: " + x + " y: " + y);
+        Console.WriteLine($"w = up, a = left, s = down, d = right");
         while (true)
         {
+            textInput = Console.ReadLine();
+            if (textInput == "w")
+            {
+                y++;
+                Console.WriteLine($"x: " + x + " y: " + y);
+            }
+            else if (textInput == "a")
+            {
+                x--;
+                Console.WriteLine($"x: " + x + " y: " + y);
+            }
+            else if (textInput == "s")
+            {
+                y--;
+                Console.WriteLine($"x: " + x + " y: " + y);
+            }
+            else if (textInput == "d")
+            {
+                x++;
+                Console.WriteLine($"x: " + x + " y: " + y);
+            }
+            if (rnd.Next(0, 11) == 1)
+            {
+                while (true)
+                {
+                    Console.WriteLine("Battle");
+                    break;
+                }
+            }
         }
     }
 }
