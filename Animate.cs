4e68b76f594b6d8079e05cc8bd9c16542d0acf2e
Markus Merits <markus.merits@hotmail.se>
2024-11-20 10:21:40 +0100

    lagt till tre metoder i animate, reformaterat en del av koden och skrivit komentarer

--- Animate.cs
+++ Animate.cs
@@ -18,4 +18,23 @@ public class animate
 		this.experiance = experiance; 
 		this.level = level;
 	}
+	// updaterar så maxHealth och maxEnergy har korrekt värden för sin level
+	public void updateMaxAttributes()
+	{
+		maxHealth = level * 10;
+		maxEnergy = level * 5;
+	}
+	// sätter currantHealth och currantEnergy till sina max värden
+	public void attributesToMax()
+	{
+		currantHealth = maxHealth;
+		currantEnergy = maxEnergy;
+	}
+	// tar bort rätt mängd experiance, ger en level och startar updateMaxAttributes()
+	public void levelUp()
+	{
+		experiance = experiance - level * 10;
+		level++;
+		updateMaxAttributes();
+    }
 }
--- Program.cs
+++ Program.cs
@@ -27,15 +27,11 @@ class Program
         int damageDealt = 0;
         int restEnergyGained = 0;
         bool playerTurnOver = false;
-        int overflow = 0;
         while (true)
         {
             if (player.experiance >= (player.level * 10))
             {
-                player.experiance = player.experiance - player.level * 10;
-                player.level++;
-                player.maxHealth = player.level * 10;
-                player.maxEnergy = player.level * 5;
+                player.levelUp();
             }
             if (player.currantHealth < 1)
             {
@@ -79,60 +75,63 @@ class Program
                         break;
                 }
                 enemyLevel = rnd.Next(1, 3);
+                // om du vill ändra max energi eller max health scaling får du göra det i animate updateMaxAttributes
                 var enemy = new animate(enemyName, enemyMaxHealth, enemyCurrantHealth, 5, 5, 0, enemyLevel);
+                enemy.updateMaxAttributes();
+                enemy.attributesToMax();
+                player.attributesToMax();
                 Console.WriteLine($"Strid med " + enemyName + " level: " + enemyLevel);
-                player.currantHealth = player.maxHealth;
-                player.currantEnergy = player.maxEnergy;
-                enemy.maxHealth = enemy.level * 10;
-                enemy.maxEnergy = enemy.level * 5;
-                enemy.currantHealth = enemy.maxHealth;
-                enemy.currantEnergy = enemy.maxEnergy;
-                
+
                 //Combat loop
                 while (true)
                 {
                     //printar fiendens och dina liv och energi värden samt de olika saker du kan göra i konsolen
                     Console.WriteLine($"////Attackera////Blocka////Vila////");
-                    Console.WriteLine($"Spelarens hp: " + player.currantHealth + " av max " + player.maxHealth);
-                    Console.WriteLine($"Spelarens energi: " + player.currantEnergy + " av max " + player.maxEnergy);
-                    Console.WriteLine($"Fiendens hp: " + enemy.currantHealth + " av max " + enemy.maxHealth);
-                    Console.WriteLine($"Fiendens energi: " + enemy.currantEnergy + " av max " + enemy.maxEnergy);
+                    Console.WriteLine($"" + player.name + " hp: " + player.currantHealth + " av max " + player.maxHealth);
+                    Console.WriteLine($"" + player.name + " energi: " + player.currantEnergy + " av max " + player.maxEnergy);
+                    Console.WriteLine($"" + enemy.name + " hp: " + enemy.currantHealth + " av max " + enemy.maxHealth);
+                    Console.WriteLine($"" + enemy.name + " energi: " + enemy.currantEnergy + " av max " + enemy.maxEnergy);
                     textInput = Console.ReadLine();
                     Console.WriteLine($"///////////////////////////////////");
+
+                    // spelarens del av combat delen av scriptet
+
+                    //randomizar ett värde och subtraherar fiendens health med det
                     if (textInput == "Attackera" && player.currantEnergy > 0)
                     {
                         damageBlockedPlayer = 0;
-                        //randomizar ett värde och subtraherar fiendens health med det
                         damageDealt = rnd.Next(1, 5);
                         enemy.currantHealth = enemy.currantHealth - (damageDealt + player.level - damageBlockedEnemy);
                         Console.WriteLine($"" + player.name + " attackerar och gör " + damageDealt + " skada");
                         playerTurnOver = true;
                         player.currantEnergy--;
                     }
+                    //randomizar ett värde och sparar det till nästa gång fienden attackerar och subtraherar skadan med värdet damageBlockedPlayer
                     else if (textInput == "Blocka" && player.currantEnergy > 0)
                     {
                         damageBlockedPlayer = 0;
-                        //randomizar ett värde och sparar det till nästa gång fienden attackerar och subtraherar skadan med värdet damageBlockedPlayer
                         damageBlockedPlayer = rnd.Next(1, 4);
                         Console.WriteLine($"" + player.name + " blockerar för " + damageBlockedPlayer + " skada");
                         playerTurnOver = true;
                         player.currantEnergy--;
                     }
+                    //randomizar ett värde och lägger till detta värde till currantEnergy
                     else if (textInput == "Vila")
                     {
-                        //randomizar ett värde och lägger till detta värde till currantEnergy
                         damageBlockedPlayer = 0;
                         restEnergyGained = rnd.Next(1, 5);
                         player.currantEnergy = player.currantEnergy + restEnergyGained;
                         //ser till att currantEnergy inte blir större än maxEnergy
                         if (player.currantEnergy > player.maxEnergy)
                         {
-                            overflow = player.maxEnergy - player.currantEnergy;
-                            player.currantEnergy = player.currantEnergy + overflow;
+                            player.currantEnergy = player.maxEnergy;
                         }
                         Console.WriteLine($"" + player.name + " vilar för " + restEnergyGained + " energi");
                         playerTurnOver = true;
                     }
+
+                    // fiendens turn del av combat delen av scriptet (om playerTurnOver == true)
+
                     //randomizar ett värde för att bestämma vad fienden ska göra
                     int enmAttack = rnd.Next(1, 4);
                     if (enmAttack == 1 && playerTurnOver && enemy.currantEnergy > 0)
@@ -145,40 +144,40 @@ class Program
                         playerTurnOver = false;
                         enemy.currantEnergy--;
                     }
+                    //randomizar ett värde och sparar det till nästa gång spelaren attackerar och subtraherar skadan med värdet damageBlockedEnemy
                     else if (enmAttack == 2 && playerTurnOver && enemy.currantEnergy > 0)
                     {
                         damageBlockedEnemy = 0;
-                        //randomizar ett värde och sparar det till nästa gång spelaren attackerar och subtraherar skadan med värdet damageBlockedEnemy
                         damageBlockedEnemy = rnd.Next(1, 4);
                         Console.WriteLine($"" + enemy.name + " blockerar för " + damageBlockedEnemy + " skada");
                         enemy.currantEnergy--;
                         playerTurnOver = false;
                     }
+                    //randomizar ett värde och ger det till fienden som currantEnergy
                     else if ((enmAttack == 3 && playerTurnOver) || (enemy.currantEnergy <= 0 && playerTurnOver))
                     {
                         damageBlockedEnemy = 0;
-                        //randomizar ett värde och ger det till fienden som currantEnergy
                         restEnergyGained = rnd.Next(1, 5);
                         enemy.currantEnergy = enemy.currantEnergy + restEnergyGained;
                         if (enemy.currantEnergy > enemy.maxEnergy)
                         {
-                            overflow = enemy.maxEnergy - enemy.currantEnergy;
-                            enemy.currantEnergy = enemy.currantEnergy + overflow;
+                            enemy.currantEnergy = enemy.maxEnergy;
                         }
                         Console.WriteLine($"" + enemy.name + " vilar för " + restEnergyGained + " energi");
                         playerTurnOver = false;
                     }
+
+                    // extra checks orelaterat till fiendens eller spelarens turns
+
                     //ser till at spelarens health går över max health
                     if (player.currantHealth > player.maxHealth)
                     {
-                        overflow = player.maxHealth - player.currantHealth;
-                        player.currantHealth = player.currantHealth + overflow;
+                        enemy.currantHealth = enemy.maxHealth;
                     }
                     //ser till at fiendens health går över max health
                     if (enemy.currantHealth > enemy.maxHealth)
                     {
-                        overflow = enemy.maxHealth - enemy.currantHealth;
-                        enemy.currantHealth = enemy.currantHealth + overflow;
+                        enemy.currantHealth = enemy.maxHealth;
                     }
                     //om fienden har noll eller mindre currantHealth dör den
                     if (enemy.currantHealth <= 0)
